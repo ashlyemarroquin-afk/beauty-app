b4b5e21679a1943e62b7d456c9c80f2e7253f66e
Ashlye Marroquin <ashlyemarroquin@gmail.com>
2025-11-10 16:25:56 -0600

    "firebaseConfig"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-import logo from './logo.svg';
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
 import './App.css';
+import { db } from "./firebase/firebaseConfig";
+import logo from './logo.svg';
+console.log("Firebase connected:", db);
+
+export default function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} =>`, doc.data());
+        });
+        console.log("✅ Firestore connection successful!");
+      } catch (error) {
+        console.error("❌ Error connecting to Firestore:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Testing Firestore connection...</Text>
+    </View>
+  );
+}
 
 function App() {
   return (
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDU2ZhiGtzpJpfwaoYaGT9-ioHK7JCTrA",
+  authDomain: "beauty-services-app-5f9b6.firebaseapp.com",
+  projectId: "beauty-services-app-5f9b6",
+  storageBucket: "beauty-services-app-5f9b6.firebasestorage.app",
+  messagingSenderId: "366390374850",
+  appId: "1:366390374850:web:33c82eee6994c99267e65b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
